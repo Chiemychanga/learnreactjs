f6f7e0b684479709c145e2099d0b2793c84be094
lchiem <larry.chiem@intel.com>
2019-03-29 11:19:19 -0700

    Good timeOfDay

---
+++ src/AppHMF.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
+
+function AppHMF() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+export default AppHMF
--- src/AppStock.test.js
+++ src/AppStock.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AppStock from './AppStock';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<AppStock />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
